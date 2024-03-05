4dd32f99ab8fc08a78490e4faf8097318a228f3b
smriti-120 <smritipyakurel7@gmail.com>
2024-03-05 08:17:02 +0545

    Assignment 1

--- obj/Debug/net8.0/Assingment1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assingment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assingment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd80400c91c6e9f98d373befa9ac62ba582f04b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assingment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assingment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
